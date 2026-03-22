5e33f4a802f5737a29ad0441f78e18d938b6e72c
Dmitry <dima.soin2014@yandex.ru>
2026-03-22 19:13:00 +0300

    создал модели User, Post, Comment

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,47 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    phone = models.CharField(max_length=15, blank=True, null=True, verbose_name='Номер телефона')
+    birth_date = models.DateField(blank=True, null=True, verbose_name='Дата рождения')
+    date_joined = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+    date_updated = models.DateTimeField(auto_now=True, verbose_name='Дата редактирования')
+
+class Meta:
+    verbose_name = 'Пользователь'
+    verbose_name_plural = 'Пользователи'
+
+def __str__(self):
+    return self.username
+
+class Post(models.Model):
+    title = models.CharField(max_length=200, verbose_name='Заголовок')
+    text = models.TextField(verbose_name='Текст')
+    image = models.ImageField(upload_to='images/', blank=True, null=True, verbose_name="Изображение")
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts', verbose_name='Автор')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Дата редактирования')
+
+    class Meta:
+        verbose_name = 'Пост'
+        verbose_name_plural = 'Посты'
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return self.title
+
+class Comment(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='comments', verbose_name='Автор')
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments', verbose_name='Пост')
+    text = models.TextField(verbose_name='Текст')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Дата редактирования')
+
+    class Meta:
+        verbose_name = 'Комментарий'
+        verbose_name_plural = 'Комментарии'
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return f'Комментарий от {self.author.username}'
